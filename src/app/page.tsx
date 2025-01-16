5be2738cc766e57a6478a21f556df2f47083ba32
ramlow07 <luamramlow@gmail.com>
2025-01-16 20:57:19 -0300

    feat: importing feedback carousel and adjusting the slides

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import BenefitsSection from "./components/BenefitsSection";
 import Footer from "./components/Footer";
+import { EmblaOptionsType } from "embla-carousel";
+import FeedbackCarousel from "./components/FeedbackCarousel";
+
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
+const imageSlides = ["/feedback1.PNG", "/feedback2.PNG", "/feedback3.PNG"];
 
 export default function Home() {
   return (
@@ -11,6 +19,7 @@ export default function Home() {
       <Menu />
       <Introduction />
       <Hero />
+      <FeedbackCarousel slides={imageSlides} options={OPTIONS} />
       <BenefitsSection />
       <Footer />
     </main>
